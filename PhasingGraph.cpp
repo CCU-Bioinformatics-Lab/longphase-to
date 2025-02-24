55863c6c17716760c5ee73d1960c8adf53f5694d
zhenyu <puddingchenk@gmail.com>
2025-02-25 00:10:21 +0800

    Enhance somatic variant phasing result handling
    - Updated edgeConnectResult method to handle somatic variant phasing
    - Added somatic variant flag to PhasingResult constructor
    - Modified read correction logic to prioritize somatic variant allele counts
    - Simplified phasing result assignment for somatic and non-somatic variants

--- PhasingGraph.cpp
+++ PhasingGraph.cpp
@@ -424,10 +424,15 @@ void VairiantGraph::edgeConnectResult(){
             h2 = special.second ;
         }
 
+        Haplotype currHP = HAPLOTYPE_UNDEFINED;
         // new block, set this position as block start 
         if( h1 == h2 ){
             // No new blocks should be created if the next SNP has already been picked up
             if( currPos < lastConnectPos ){
+                if( variantIter->second.origin == SOMATIC ){
+                    PhasingResult phasingResult(HAPLOTYPE_UNDEFINED, blockStart, variantIter->second.type, variantIter->second.origin == SOMATIC);
+                    posPhasingResult->emplace(currPos, phasingResult);
+                }
                 continue;
             }
             // check block size to remove one node island
@@ -435,14 +440,13 @@ void VairiantGraph::edgeConnectResult(){
                 posPhasingResult->erase(blockStart);
             }
             blockStart = currPos;
-            PhasingResult phasingResult(HAPLOTYPE1, blockStart, variantIter->second.type);
-            posPhasingResult->emplace(currPos, phasingResult);
+            currHP = HAPLOTYPE1;
         }
         else{
-            Haplotype currHP = ( h1 > h2 ? HAPLOTYPE1 : HAPLOTYPE2 );
-            PhasingResult phasingResult(currHP, blockStart, variantIter->second.type);
-            posPhasingResult->emplace(currPos, phasingResult);
+            currHP = ( h1 > h2 ? HAPLOTYPE1 : HAPLOTYPE2 );
         }
+        PhasingResult phasingResult(currHP, blockStart, variantIter->second.type, variantIter->second.origin == SOMATIC);
+        posPhasingResult->emplace(currPos, phasingResult);
         
         // Check if there is no edge from current node
         std::map<int,VariantEdge*>::iterator edgeIter = edgeList->find( currPos );
@@ -940,6 +944,10 @@ void VairiantGraph::readCorrection(){
         double hp2Alt = (*hpAlleleCountMap)[1][position][1];
         double result1reads = hp1Ref + hp2Alt;
         double result2reads = hp2Ref + hp1Alt;
+        if(posPhasingResultIter->second.somatic){
+            result1reads = hp2Alt;
+            result2reads = hp1Alt;
+        }
         double resultConfidence = std::max(result1reads, result2reads) / (result1reads + result2reads);
         
         Haplotype refHaplotypeResult = HAPLOTYPE_UNDEFINED;
